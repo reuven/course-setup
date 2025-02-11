10f5873bd2420ffd85816d6bfe985c764c095062
Reuven Lerner <reuven@lerner.co.il>
2025-02-11 16:13:54 +0200

    Initial commit

---
+++ retire-course.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from github import Github
+import argparse
+import datetime
+import os
+import shutil
+import subprocess
+
+
+github_token = open('/Users/reuven/.github_token').read().strip()
+
+# Get the directory of the course we want to retire
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dirname', required=True)
+args = parser.parse_args()
+dirname = args.dirname
+
+# ------------------------------------------------------------
+# Make the Git repo private
+# Switch to that directory
+print(f'Now retiring repo in {dirname}')
+os.chdir(dirname)
+wd = os.getcwd()
+print(f'Now in directory {wd}')
+
+# Get the GitHub repo for the current directory
+full_reponame = subprocess.run('git config remote.origin.url',
+                               shell=True, capture_output=True).stdout.decode().strip()
+print(f'{full_reponame=}')
+
+before, after = full_reponame.split('/')
+repo_name = 'reuven/' + after.split('.')[0]
+
+# Connect to GitHub
+g = Github(github_token)
+
+# get the repo from the current directory!
+print(repo_name)
+repo = g.get_repo(repo_name)
+print(repo)
+
+repo.edit(private=True)
+
+# ------------------------------------------------------------
+# Move the directory to the archive
+os.chdir('..')
+wd = os.getcwd()
+print(f'Now in directory {wd}')
+
+year = datetime.datetime.now().year
+
+shutil.move(
+    dirname, f'/Users/reuven/Courses/Python/Archive/{year}/')
+
+print(f'Successfully moved to {dirname}')
---
+++ setup-course.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import argparse
+import shutil
+import os
+from github import Github
+
+github_token = open('/Users/reuven/.github_token').read().strip()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', '--source', default='generic')
+parser.add_argument('-d', '--date', required=True)
+parser.add_argument('-c', '--client', required=True)
+parser.add_argument('-r', '--repo', required=True)
+parser.add_argument('-n', '--name', default='')
+
+args = parser.parse_args()
+
+if args.name:
+    suffix = f'-{args.name}'
+else:
+    suffix = ''
+
+destination = f'{args.client}-{args.date}{suffix}'
+
+print(f'Copying from "{args.source}" to "{destination}"')
+
+shutil.copytree(args.source, destination)
+
+
+os.rename(f'{destination}/Course notebook.ipynb',
+          f'{destination}/{args.client} - {args.date}{suffix}.ipynb')
+
+
+remote_info = f"""
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+        ignorecase = true
+        precomposeunicode = true
+[remote "origin"]
+        url = git@github.com:reuven/{args.repo}.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+        remote = origin
+        merge = refs/heads/main
+"""
+
+
+# Write the remote info to the Git configuration file
+with open(f'{destination}/.git/config', 'w') as outfile:
+    outfile.write(remote_info)
+
+# Create the repo on GitHub
+g = Github(github_token)
+user = g.get_user()
+repo = user.create_repo(
+    name=args.repo,
+    private=False
+)
